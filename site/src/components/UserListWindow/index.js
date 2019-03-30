8b2c0c6513e482828b2737e26a1b0c56656be1a4
rai16 <anurag16j@gmail.com>
2019-03-31 02:01:30 +0530

    added user search feature

--- site/src/components/UserListWindow/index.js
+++ site/src/components/UserListWindow/index.js
@@ -4,31 +4,44 @@ import UserDetails from '../UserDetails';
 import UserSearch from '../UserSearch';
 
 class UserListWindow extends Component {
+    
     constructor(props){
         super(props);
         this.state = {
-            selectedUser: null,
-            selectedKey: -1
+            selectedUser: {_id: -1},
+            userFilter: props.users,
+            userSearchText: ''
         }
         this.changeSelectedUser = this.changeSelectedUser.bind(this);
+        this.changeUserSearchText = this.changeUserSearchText.bind(this);
     }
     
+    
+    changeUserSearchText(e){
+        var text = e.target.value.trim();
+        var finalUserList = this.props.users;
+        if(text.length>0)
+            finalUserList = finalUserList.filter((item) => item.username.includes(text));
+        
+        this.setState({userFilter: finalUserList});
+        this.forceUpdate();
+    }
+
     changeSelectedUser(key, user){
-        if(this.state.selectedKey!==key){
-            this.setState({selectedUser: user, selectedKey: key});
+        if(this.state.selectedUser._id!==user._id){
+            this.setState({selectedUser: user});
             this.props.onSelectedUserChange(user);
         }
     }
 
     render(){
-    
         return(
             <div className='inbox_people'>
-                <UserSearch/>
+                <UserSearch onSearchTextChange = {this.changeUserSearchText}/>
                 <div className='inbox_chat'>
-                    {this.props.users.map((user, key) => {
+                    {this.state.userFilter.map((user, key) => {
                         return <div key = {key} onClick = {() => this.changeSelectedUser(key, user)}> 
-                            <UserDetails key = {key} isActive = {this.state.selectedKey === key} user = {user}/>
+                            <UserDetails key = {key} isActive = {this.state.selectedUser._id === user._id} user = {user}/>
                         </div>
                     })}
                 </div>
--- site/src/components/UserSearch/index.js
+++ site/src/components/UserSearch/index.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import '../home.scss';
-
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+library.add(faSearch);
 class UserSearch extends Component {
     render(){
         return(
             <div className='headind_srch'>
-                <div className='recent_heading'>
-                  <h4>Recent</h4>
+                <div className='form-group d-flex flex-row'>
+                  <input
+                    placeholder = 'Search'
+                    type = 'text'
+                    className = 'form-control user-search'
+                    onKeyUp = {(e) => this.props.onSearchTextChange(e)}
+                  />
+                  <FontAwesomeIcon className = "search-icon fa-lg" icon="search"  />
                 </div>
             </div>
         );
